7c97beba49bdd571d5899229fccdd96399ad5f16
Nikolai_Yurchenko <zoyberlo@gmail.com>
2021-03-27 17:04:45 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html><head> 
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Моя первый PHP скрипт</title>
+</head>
+<body>
+<?	
+echo "<h1>Привет! Я PHP скрипт!</h1>"; 
+?>
+</body>
+</html>
