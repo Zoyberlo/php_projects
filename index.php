50cb26780488047d24e6dd59a41180b309fc69a1
Nikolai_Yurchenko <zoyberlo@gmail.com>
2021-03-28 21:57:21 +0300

    Rename "Exercise" to "Task" and added 5 - 8 new Tasks

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$ExampleNumb = 1;
+$TaskNumb = 1;
 
-echo "<h1>Задача $ExampleNumb.</h1> Дана строка 'minsk'. Сделайте из нее строку 'MINSK'.<br><br>
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'minsk'. Сделайте из нее строку 'MINSK'.<br><br>
 
     Решение:<br><br>"; 
 
@@ -10,11 +10,11 @@ $str = 'minsk';
 
 echo  'Было: ' . $str . '<br>Стало: ' . strtoupper($str) . '<br>';
 
-$ExampleNumb++;   
+$TaskNumb++;   
 
-//Example 1
+//Task 1
 
-echo "<h1>Задача $ExampleNumb.</h1> Дана строка 'минск'. Сделайте из нее строку 'МИНСК'.<br><br>
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'минск'. Сделайте из нее строку 'МИНСК'.<br><br>
 
     Решение:<br><br>"; 
 
@@ -22,11 +22,11 @@ $str = 'минск';
 
 echo  'Было: ' . $str . '<br>Стало: ' . mb_strtoupper($str) . '<br>';
 
-$ExampleNumb++;  
+$TaskNumb++;  
 
-//Example 2
+//Task 2
 
-echo "<h1>Задача $ExampleNumb.</h1> Дана строка 'MINSK'. Сделайте из нее строку 'Minsk'.<br><br>
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'MINSK'. Сделайте из нее строку 'Minsk'.<br><br>
 
     Решение:<br><br>";
     
@@ -34,11 +34,11 @@ $str = 'MINSK';
 
 echo  'Было: ' . $str . '<br>Стало: ' . ucfirst(strtolower($str)) . '<br>';
 
-$ExampleNumb++;  
+$TaskNumb++;  
 
-//Example 3
+//Task 3
 
-echo "<h1>Задача $ExampleNumb.</h1> В переменной \$date лежит дата в формате '31-12-2030'. Преобразуйте эту дату в формат '2030.12.31''.<br><br>
+echo "<h1>Задача $TaskNumb.</h1> В переменной \$date лежит дата в формате '31-12-2030'. Преобразуйте эту дату в формат '2030.12.31''.<br><br>
 
     Решение:<br><br>";
 
@@ -55,8 +55,53 @@ $date[2] = '.';
 $date[5] = '.';
 echo '<br>Стало: ' . $date . '<br>';
 
-unset($date, $arr, $ExampleNumb, $str);  
+unset($date, $arr, $str); 
 
-//Example 4
+//Task 4
 
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'london'. Сделайте из нее строку 'London'.<br><br>
 
+    Решение:<br><br>";
+    
+$str = 'london';
+
+echo  'Было: ' . $str . '<br>Стало: ' . ucfirst($str) . '<br>';
+
+$TaskNumb++;  
+
+//Task 5
+
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'London'. Сделайте из нее строку 'london'.<br><br>
+
+    Решение:<br><br>";
+    
+$str = 'London';
+
+echo  'Было: ' . $str . '<br>Стало: ' . lcfirst($str) . '<br>';
+
+$TaskNumb++;  
+
+//Task 6
+
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'london is the capital of great britain'.<br>
+    Сделайте из нее строку 'London Is The Capital Of Great Britain'.<br><br>
+    Решение:<br><br>";
+    
+$str = 'london is the capital of great britain';
+
+echo  'Было: ' . $str . '<br>Стало: ' . ucwords($str) . '<br>';
+
+$TaskNumb++;  
+
+//Task 7
+
+echo "<h1>Задача $TaskNumb.</h1> Дана строка 'LONDON'. Сделайте из нее строку 'London'.<br><br>
+    Решение:<br><br>";
+    
+$str = 'LONDON';
+
+echo  'Было: ' . $str . '<br>Стало: ' . ucfirst(strtolower($str)) . '<br>';
+
+$TaskNumb++;  
+
+//Task 8
