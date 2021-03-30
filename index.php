5e2df72871ce67118dac1d427bdf8d5f0b664a7c
Nikolai_Yurchenko <zoyberlo@gmail.com>
2021-03-30 23:14:05 +0300

    Added tasks 17 - 24

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <header>
     <a href="index.php">Главная страница</a> |
-    <a href="Tasks\str_tasks.php">Работа со строками</a>
+    <a href="Tasks/str_tasks.php">Работа со строками</a>
 </header>
 
 <h1>Главная страница</h1>
@@ -8,5 +8,3 @@
 <?php
 
 echo '<p>Страница для перехода по заданиям.</p>';
-
-?>
